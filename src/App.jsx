5e91e12ff746985ce6f4224e6e86fc94caf8c776
Shashank Singh <ec22267@qmul.ac.uk>
2025-01-22 19:08:38 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,27 @@
-import { useState } from "react";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+// src/App.js
+import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Hero from "./components/Hero";
-import Stats from "./components/Stats";
-import FAQ from "./components/FAQ";
-import Tables from "./components/Tables";
-import { ContactUs } from "./components/ContactUs";
-import Home from "./pages/Home";
+import HomePage from "./pages/HomePage";
+import Expertise from "./pages/Expertise";
+import Industries from "./pages/Industries";
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
 
 function App() {
 	return (
-		<div className="bg-gray-200">
-			<Routes>
-				<Route exact path="/" element={<Home />} />
-			</Routes>
-			{/* <Header />
-			<Hero />
-			<Tables />
-			Stats, FAQ and Footer Section
-			<Stats />
-			<FAQ />
+		<div className="min-h-screen flex flex-col">
+			<div className="text-red-500 text-center">Hello, Tailwind!</div>
+			<Header />
+			<main className="flex-grow">
+				{/* Add routing here if using react-router-dom */}
+				<HomePage />
+				<Expertise />
+				<Industries />
+				<AboutUs />
+				<ContactUs />
+			</main>
 			<Footer />
-			<ContactUs /> */}
 		</div>
 	);
 }
